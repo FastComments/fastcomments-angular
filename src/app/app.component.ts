605f9c051e713122e9e5bfbef7102e2287695e08
winrid <winrid@gmail.com>
2021-04-20 07:32:59 -0700

    updating config version

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {FastCommentsCommentWidgetConfig} from "fastcomments-typescript";
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'fastcomments-angular-workspace';
+  config: FastCommentsCommentWidgetConfig = {
+    tenantId: 'demo',
+  };
 }
