966082c85864f580afabd2956e69f1e97c4ccb8c
winrid <winrid@gmail.com>
2026-07-09 22:55:53 -0700

    Upgrade Angular 20 -> 21 (ng update migrations, control-flow syntax)

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,9 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { enableDebugTools } from '@angular/platform-browser';
-import {ApplicationRef} from '@angular/core';
+import {ApplicationRef, provideZoneChangeDetection} from '@angular/core';
 import { AppModule } from './app/app.module';
 
-platformBrowserDynamic().bootstrapModule(AppModule).then(module => {
+platformBrowserDynamic().bootstrapModule(AppModule, { applicationProviders: [provideZoneChangeDetection()], }).then(module => {
   const appRef = module.injector.get(ApplicationRef);
   const compRef = appRef.components[0];
   enableDebugTools(compRef);
